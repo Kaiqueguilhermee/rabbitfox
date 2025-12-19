686c2c72a486551f7ac6694e461ddd35e4595cea
Kaiqueguilhermee <201179565+Kaiqueguilhermee@users.noreply.github.com>
2025-12-19 16:58:09 -0300

    test

--- resources/views/includes/navbar_left.blade.php
+++ resources/views/includes/navbar_left.blade.php
@@ -1,11 +1,6 @@
 <!-- Bootstrap Offcanvas Sidebar -->
 <div>
     <!-- Toggle button (mobile) -->
-    <button class="text-heading bg-transparent border border-transparent hover:bg-neutral-secondary-medium focus:ring-4 focus:ring-neutral-tertiary font-medium leading-5 rounded-md ms-3 mt-3 text-sm p-2 focus:outline-none inline-flex sm:hidden" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasSidebar" aria-controls="offcanvasSidebar">
-        <span class="sr-only">Open sidebar</span>
-        <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h10"/></svg>
-    </button>
-
     <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasSidebar" aria-labelledby="offcanvasSidebarLabel">
         <div class="offcanvas-header">
             <h5 class="offcanvas-title" id="offcanvasSidebarLabel">Menu</h5>
--- resources/views/includes/navbar_top.blade.php
+++ resources/views/includes/navbar_top.blade.php
@@ -5,10 +5,10 @@
                 <img src="{{ asset('storage/'.config('setting')['software_logo_white']) }}" alt="" style="max-width:100%; height: 50px">
             </a>
 
-<button class="text-heading bg-transparent border border-transparent hover:bg-neutral-secondary-medium focus:ring-4 focus:ring-neutral-tertiary font-medium leading-5 rounded-md ms-3 mt-3 text-sm p-2 focus:outline-none inline-flex sm:hidden" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasSidebar" aria-controls="offcanvasSidebar">
+*/      <button class="text-heading bg-transparent  focus:ring-4 focus:ring-neutral-tertiary font-medium leading-5 rounded-md ms-3 mt-3 text-sm p-2 focus:outline-none inline-flex sm:hidden" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasSidebar" aria-controls="offcanvasSidebar">
         <span class="sr-only">Open sidebar</span>
-        <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h10"/></svg>
-    </button>
+                <img src="{{ asset('/assets/images/svg/menu-nav.svg') }}" alt="" width="28">
+            </button>
         </div>
 
         @if(!empty(config('setting')['instagram']))
